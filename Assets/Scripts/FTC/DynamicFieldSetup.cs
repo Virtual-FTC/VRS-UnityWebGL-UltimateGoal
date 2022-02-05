cd98b47ccda35e0ae1f876b6b6d966a49480492a
Emilia Reyes <emi.reyes243@gmail.com>
2022-02-05 18:01:22 -0500

    dynamic field bugfixes

--- Assets/Scripts/FTC/DynamicFieldSetup.cs
+++ Assets/Scripts/FTC/DynamicFieldSetup.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 public class DynamicFieldSetup : MonoBehaviour
 {
     private void Start()
     {
-        Debug.Log("DYNAMIC FIELD SETUP");
-
-        foreach (PhotonView pv in GetComponentsInChildren<PhotonView>())
+        foreach (RingController ring in GetComponentsInChildren<RingController>())
         {
-            pv.gameObject.transform.parent = null;
+            Transform ringTrans = ring.gameObject.transform;
+            PhotonNetwork.Destroy(ring.GetComponent<PhotonView>());
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Ring"), ringTrans.position, Quaternion.identity);
+        }
+        foreach (BlueWobble wobble in GetComponentsInChildren<BlueWobble>())
+        {
+            Transform wobTrans = wobble.gameObject.transform;
+            PhotonNetwork.Destroy(wobble.GetComponent<PhotonView>());
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "BlueWobble"), wobTrans.position, Quaternion.identity);
+        }
+        foreach (RedWobble wobble in GetComponentsInChildren<RedWobble>())
+        {
+            Transform wobTrans = wobble.gameObject.transform;
+            PhotonNetwork.Destroy(wobble.GetComponent<PhotonView>());
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "RedWobble"), wobTrans.position, Quaternion.identity);
         }
     }
 }
