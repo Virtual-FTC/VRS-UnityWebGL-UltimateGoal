f092c7bde67223a817940ecdd9cf2d6b087de50d
Emilia Reyes <emi.reyes243@gmail.com>
2022-02-16 17:18:57 -0500

    reverting wobble RPCs

--- Assets/Scripts/FTC/GameControl/RedWobble.cs
+++ Assets/Scripts/FTC/GameControl/RedWobble.cs
@@ -1,14 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 
 public class RedWobble : MonoBehaviour
 {
-    [PunRPC]
-    public void swapPhotonViews(bool toEnable)
-    {
-        GetComponent<PhotonRigidbodyView>().enabled = toEnable;
-        GetComponent<PhotonTransformView>().enabled = !toEnable;
-    }
+
 }
