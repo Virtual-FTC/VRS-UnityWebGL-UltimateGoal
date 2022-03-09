12576a456f73bfa64d62fd49f0b64ed99e8c3aea
Emilia Reyes <emi.reyes243@gmail.com>
2022-03-09 15:51:27 -0500

    fixed dynamic field setup & disabled ring photon transform view

--- Assets/Scripts/FTC/DynamicFieldSetup.cs
+++ Assets/Scripts/FTC/DynamicFieldSetup.cs
@@ -6,15 +6,19 @@ using System.IO;
 
 public class DynamicFieldSetup : MonoBehaviour
 {
+    public GameObject[] prefabs;
+
     private void Start()
-    {   
+    {
         foreach (RingController ring in GetComponentsInChildren<RingController>())
         {
-            Transform ringTrans = ring.gameObject.transform;            
+            Transform ringTrans = ring.gameObject.transform;
             Destroy(ring.gameObject);
-
+                                                
             if (PhotonNetwork.IsMasterClient)
                 PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Ring"), ringTrans.position, Quaternion.identity);
+            else if (!PhotonNetwork.IsConnected)
+                Instantiate(prefabs[0], ringTrans.position, Quaternion.identity);
         }
         
         foreach (BlueWobble wobble in GetComponentsInChildren<BlueWobble>())
@@ -24,6 +28,8 @@ public class DynamicFieldSetup : MonoBehaviour
 
             if (PhotonNetwork.IsMasterClient)
                 PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "BlueWobble"), wobTrans.position, Quaternion.identity);
+            else if (!PhotonNetwork.IsConnected)
+                Instantiate(prefabs[1], wobTrans.position, Quaternion.identity);
         }
         
         foreach (RedWobble wobble in GetComponentsInChildren<RedWobble>())
@@ -33,6 +39,8 @@ public class DynamicFieldSetup : MonoBehaviour
 
             if (PhotonNetwork.IsMasterClient)
                 PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "RedWobble"), wobTrans.position, Quaternion.identity);
+            else if (!PhotonNetwork.IsConnected)
+                Instantiate(prefabs[2], wobTrans.position, Quaternion.identity);
         }
     }
 }
