80b9a895a821099914d0af85202a3c9894c859e5
Emilia REyes <emi.reyes243@gmail.com>
2022-05-19 19:00:29 -0400

    distance sensor initial setup, modified colorSensor for distance

---
+++ Assets/Scripts/Robot/Sensors/DistanceSensor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceSensor : MonoBehaviour//, ISensor
+{
+    Ray rayToSenseDistance;
+    RaycastHit hit;
+    Collider detectedCollider;
+
+    public float rayLength;
+
+    private float distanceSensed = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayToSenseDistance = new Ray(transform.position, transform.forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetRayOriginAndDirection();
+        DetectObjectInFront();
+    }
+
+    private void SetRayOriginAndDirection()
+    {
+        rayToSenseDistance.origin = transform.position;
+        rayToSenseDistance.direction = transform.forward;
+    }
+
+    private void DetectObjectInFront()
+    {
+        if (Physics.Raycast(rayToSenseDistance, out hit, rayLength))
+        {
+            detectedCollider = hit.collider;
+            if (detectedCollider.GetComponent<Renderer>() == null)
+            {
+                Debug.Log("Distance Sensor is sensing an unrendered object");
+                return;
+            }
+            distanceSensed = hit.distance;
+            print("distance sensed: " + distanceSensed);
+        }
+        else
+            distanceSensed = -1;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(rayToSenseDistance.origin, rayToSenseDistance.direction, Color.red);
+    }
+}
