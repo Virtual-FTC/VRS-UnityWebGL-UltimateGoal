8da4a48c30a2bec8c4c95f13a4f96a9f38dd0824
Emilia REyes <emi.reyes243@gmail.com>
2022-05-20 16:31:04 -0400

    confirmed distance sensor length is 2m & is now adjustable

--- Assets/Scripts/Robot/Sensors/DistanceSensor.cs
+++ Assets/Scripts/Robot/Sensors/DistanceSensor.cs
@@ -8,14 +8,18 @@ public class DistanceSensor : MonoBehaviour//, ISensor
     RaycastHit hit;
     Collider detectedCollider;
 
+    [Tooltip("value in centimeters")]
     public float rayLength;
 
     private float distanceSensed = -1;
+    private float convertedRayLength;
+    private static float fieldScaleFactor = 200f;//field appears to be at half scale (value should be 100f at full scale)
 
     // Start is called before the first frame update
     void Start()
     {
         rayToSenseDistance = new Ray(transform.position, transform.forward);
+        convertedRayLength = rayLength / fieldScaleFactor;
     }
 
     // Update is called once per frame
@@ -29,14 +33,15 @@ public class DistanceSensor : MonoBehaviour//, ISensor
     {
         rayToSenseDistance.origin = transform.position;
         rayToSenseDistance.direction = transform.forward;
+
     }
 
     private void DetectObjectInFront()
     {
-        if (Physics.Raycast(rayToSenseDistance, out hit, rayLength))
+        if (Physics.Raycast(rayToSenseDistance, out hit, convertedRayLength))
         {
-            distanceSensed = hit.distance;
-            print("distance sensed: " + distanceSensed);
+            distanceSensed = hit.distance * fieldScaleFactor;
+            print(hit.transform + "distance sensed: " + distanceSensed);
         }
         else
             distanceSensed = -1;
@@ -45,6 +50,6 @@ public class DistanceSensor : MonoBehaviour//, ISensor
 
     private void OnDrawGizmos()
     {
-        Debug.DrawRay(rayToSenseDistance.origin, rayToSenseDistance.direction, Color.red);
+        Debug.DrawRay(rayToSenseDistance.origin, rayToSenseDistance.direction * convertedRayLength, Color.red);
     }
 }
