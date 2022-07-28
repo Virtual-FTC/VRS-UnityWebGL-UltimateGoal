aa655b80d58951332b38199db2d3f6c4e48673c7
S3TO <ninja00000001@gmail.com>
2022-07-28 09:44:56 -0700

    White line scoring now keeps track locally of how long player has been on the white line and adds points accordingly.

--- Assets/Scripts/FTC/GameControl/WhiteLineGoal.cs
+++ Assets/Scripts/FTC/GameControl/WhiteLineGoal.cs
@@ -9,6 +9,8 @@ public class WhiteLineGoal : MonoBehaviour
     public string tagOfGameObject2 = "RedRobot";
 
     private bool inZone = false;
+    private bool pointsAdded = false;
+    private float timeInZone = 0.0f;
 
     private GameTimer gameTimer;
 
@@ -19,31 +21,95 @@ public class WhiteLineGoal : MonoBehaviour
 
     void OnTriggerEnter(Collider collision)
     {
-        if (PhotonNetwork.IsConnected && !PhotonNetwork.IsMasterClient)
-            return;
-        if ((collision.tag == tagOfGameObject1 || collision.tag == tagOfGameObject2) && inZone == false && gameTimer.getGameType() == "auto")
+        if(PhotonNetwork.IsConnected)
         {
-            inZone = true;
-
-            if (collision.tag == tagOfGameObject1)
-                ScoreKeeper._Instance.addScoreBlue(ScoreKeeper._Instance.WhiteLineGoal);
-            else
-                ScoreKeeper._Instance.addScoreRed(ScoreKeeper._Instance.WhiteLineGoal);
+            if(collision.tag == "Player")
+            {
+                inZone = true;
+            }
         }
+        else
+        {
+            if(collision.tag == "Player")
+            {
+                inZone = true;
+            }
+        }
+    }
+    void OnTriggerStay(Collider collision)
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            if(inZone)
+            {
+                if(!pointsAdded)
+                {
+                    timeInZone += Time.fixedDeltaTime;
+                    if(timeInZone > 5.0f && !pointsAdded)
+                    {
+                        pointsAdded = true;
+                        object team;
+                        PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(PLAYERPROPS.PLAYER_TEAM,out team);
+                        if((int)team == 0)
+                            ScoreKeeper._Instance.addScoreBlue(ScoreKeeper._Instance.WhiteLineGoal);
+                        else
+                            ScoreKeeper._Instance.addScoreRed(ScoreKeeper._Instance.WhiteLineGoal);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if(inZone)
+            {
+                if(!pointsAdded)
+                {
+                    timeInZone += Time.fixedDeltaTime;
+                    if(timeInZone > 5.0f && !pointsAdded)
+                    {
+                        pointsAdded = true;
+                        ScoreKeeper._Instance.addScoreBlue(ScoreKeeper._Instance.WhiteLineGoal);
+                    }
+                }
+            }
+        }
+
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        if (PhotonNetwork.IsConnected && !PhotonNetwork.IsMasterClient)
-            return;
-        if ((collision.tag == tagOfGameObject1 || collision.tag == tagOfGameObject2) && inZone == false && gameTimer.getGameType() == "auto")
+        if(PhotonNetwork.IsConnected)
         {
-            inZone = false;
+            if(collision.tag == "Player")
+            {
+                inZone = false;
+                timeInZone = 0.0f;
+                if(pointsAdded)
+                {
+                    pointsAdded = false;
+                    object team;
+                    PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(PLAYERPROPS.PLAYER_TEAM,out team);
+                    if((int)team == 0)
+                        ScoreKeeper._Instance.addScoreBlue(-ScoreKeeper._Instance.WhiteLineGoal);
+                    else
+                        ScoreKeeper._Instance.addScoreRed(-ScoreKeeper._Instance.WhiteLineGoal);
 
-            if (collision.tag == tagOfGameObject1)
-                ScoreKeeper._Instance.addScoreBlue(-ScoreKeeper._Instance.WhiteLineGoal);
-            else
-                ScoreKeeper._Instance.addScoreRed(-ScoreKeeper._Instance.WhiteLineGoal);
+                }
+            }
         }
+        else
+        {
+            if(collision.tag == "Player")
+            {
+                inZone = false;
+                timeInZone = 0.0f;
+                if(pointsAdded)
+                {
+                    pointsAdded = false;
+                    ScoreKeeper._Instance.addScoreBlue(-ScoreKeeper._Instance.WhiteLineGoal);
+                }
+            }
+        }
+
     }
 }
