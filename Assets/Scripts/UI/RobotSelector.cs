f6f583f75a92aa3301e738829771e394e3eb5ea5
S3TO <ninja00000001@gmail.com>
2022-07-28 14:02:58 -0700

    Added robot selection to the field2020 scene

---
+++ Assets/Scripts/UI/RobotSelector.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using  System.IO;
+using UnityEngine;
+using Photon.Pun;
+
+public class RobotSelector : MonoBehaviour
+{
+    public string[] prefabNames;
+    public GameObject[] spawnPositions;
+
+    private bool isDisplaying = false;
+    public GameObject displayButton;
+    public GameObject buttonGroup;
+
+    public GameObject[] robotPrefabs;
+    void Start()
+    {
+        buttonGroup.SetActive(false);
+    }
+    public void toggleRobotOptions()
+    {
+        isDisplaying = !isDisplaying;
+        displayButton.SetActive(!isDisplaying);
+        buttonGroup.SetActive(isDisplaying);
+    }
+    public void changeRobot(int robotType)
+    {
+        if(PhotonNetwork.IsConnected)
+        {
+            foreach(GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if(player.GetComponent<PhotonView>().Owner == PhotonNetwork.LocalPlayer)
+                {
+                    PhotonNetwork.Destroy(player);
+                }
+            }
+            int spawnPos = (int)PhotonNetwork.LocalPlayer.CustomProperties[PLAYERPROPS.PLAYER_POS];
+
+
+            var robot = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabNames[robotType]), spawnPositions[spawnPos].transform.position, spawnPositions[spawnPos].transform.rotation, 0);
+            robot.GetComponent<RobotController>().setStartPosition(spawnPositions[spawnPos].transform);
+
+            toggleRobotOptions();
+        }
+        else
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+
+            int spawnPos = 0;
+            var robot = Instantiate(robotPrefabs[robotType], spawnPositions[spawnPos].transform.position, spawnPositions[spawnPos].transform.rotation);
+
+            toggleRobotOptions();
+        }
+    }
+}
